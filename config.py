421b2f6a981b01d2f8a85c0f09693f11954e6cdf
Marvellous Akomolafe <akomolafemarvellous@gmail.com>
2022-01-12 00:11:30 +0100

    packaged the functions into a class

--- config.py
+++
@@ -1,4 +0,0 @@
-url = "https://itdashboard.gov"
-locator = "xpath://*[@id='node-23']"
-workbook_name = "output/IT_Dashboard.xlsx"
-worksheet_name = "Agencies"
--- task.py
+++ task.py
@@ -1,20 +1,13 @@
-from main import *
-from config import *
+from main import ITDashboard
+
+robot = ITDashboard()
 
 
 def main():
-    try:
-        dive_into_website(url, locator)
-        create_workbook(workbook_name)
-        create_worksheet(worksheet_name)
-        scrape_agency_data_populate_records(worksheet_name)
-        get_individual_investment_colnames()
-        scrape_individual_investment()
-        download_business_case_pdf()
-        browser.close_all_browsers()
-        compare_uii_and_investment_title(workbook_name)
-    finally:
-        minimal_task()
+    robot.scrape_agency_data()
+    robot.scrape_individual_investment()
+    robot.compare_uii_and_title()
+    robot.save_workbook()
 
 
 if __name__ == "__main__":
